5fd7a16038f57c8887731f2d0015c472800e24b3
Dong Tian <dongtianzen@gmail.com>
2017-01-07 12:48:27 -0500

    dev _entity_fields_info

--- modules/custom/phpdebug/create_entity_reference_field.php
+++ modules/custom/phpdebug/create_entity_reference_field.php
@@ -2,16 +2,14 @@
 
 /**
  *
-  require_once(DRUPAL_ROOT . '/modules/demo/debugcode/create_entity_reference_field.php');
-  _entity_create_field(3);
-  _query_nodes();
+  require_once(DRUPAL_ROOT . '/modules/custom/phpdebug/create_entity_reference_field.php');
+  _run_batch_entity_create_fields();
  */
-
 
 function _run_batch_entity_create_fields() {
   $entity_info = array(
-    'entity_type' => 'node',  // 'taxonomy_term', 'user'
-    'bundle' => 'page',
+    'entity_type' => 'taxonomy_term',  // 'node', 'taxonomy_term', 'user'
+    'bundle' => 'client',
   );
 
   $fields = _entity_fields_info();
@@ -23,7 +21,11 @@ function _run_batch_entity_create_fields() {
 
 function _entity_fields_info() {
   $fields[] = array(
-    'field_name' => 'field_page_large_text',
+    // field_storage_config
+    'field_name' => 'field_client_contactname',
+    'type'        => 'text_long',       // 'entity_reference', 'image', 'text_with_summary',
+    // field_config
+    'label'       => t('Contact Name'),
   );
 
   return $fields;
@@ -38,26 +40,26 @@ function _entity_create_fields($entity_info, $field) {
 
   entity_create('field_config', array(
     'field_name'  => $field['field_name'],
-    'label'       => 'Large Text',
+    'label'       => $field['label'],
     'entity_type' => $entity_info['entity_type'],
     'bundle'      => $entity_info['bundle'],
     // optional
-    'required'    => FALSE,
-    'description' => t('your description'),
+    // 'required'    => isset($field['required']) ? TRUE : FALSE,
+    // 'description' => isset($field['description']) ? $field['description'] : NULL,
   ))->save();
 }
 
 function _entity_create_field_template() {
   entity_create('field_storage_config', array(
     'field_name'  => 'field_page_large_text',
-    'entity_type' => 'node',          // 'taxonomy_term'
+    'entity_type' => 'node',                 // 'taxonomy_term'
     'type'        => 'text_with_summary',    // 'entity_reference', 'image'
   ))->save();
 
   entity_create('field_config', array(
     'field_name'  => 'field_page_large_text',
     'label'       => 'Large Text',
-    'entity_type' => 'node',          // 'taxonomy_term'
+    'entity_type' => 'node',                 // 'taxonomy_term'
     'bundle'      => 'page',
     // optional
     'required'    => FALSE,
