2ba16eab8092f1c9292d1443e9b5e2c11f80c821
Dong Tian <dongtianzen@gmail.com>
2017-01-07 18:54:05 -0500

    dev _run_batch_entity_create_terms

--- modules/custom/phpdebug/create_entity_term.php
+++ modules/custom/phpdebug/create_entity_term.php
@@ -6,6 +6,8 @@
   _run_batch_entity_create_terms();
  */
 
+use Drupal\taxonomy\Entity\Term;
+
 function _run_batch_entity_create_terms() {
   $vocabulary = array(
     'vid'  => 'province',
@@ -13,7 +15,7 @@ function _run_batch_entity_create_terms() {
 
   $terms = _entity_terms_info();
   foreach ($terms as $term) {
-    _entity_create_terms($term);
+    _entity_create_terms($vocabulary, $term);
   }
 }
 
@@ -21,39 +23,39 @@ function _entity_terms_info() {
   $terms = array(
     '北京',
     '上海',
-    // '天津',
-    // '重庆',
-    // '内蒙古',
-    // '辽宁',
-    // '吉林',
-    // '黑龙江',
-    // '河北',
-    // '山东',
-    // '山西',
-    // '河南',
-    // '江苏',
-    // '安徽',
-    // '浙江',
-    // '江西',
-    // '湖北',
-    // '湖南',
-    // '福建',
-    // '广东',
-    // '广西',
-    // '海南',
-    // '贵州',
-    // '云南',
-    // '四川',
-    // '陕西',
-    // '甘肃',
-    // '宁夏',
-    // '青海',
-    // '新疆',
-    // '西藏',
-    // '香港',
-    // '澳门',
-    // '台湾',
-    // '其它地区',
+    '天津',
+    '重庆',
+    '内蒙古',
+    '辽宁',
+    '吉林',
+    '黑龙江',
+    '河北',
+    '山东',
+    '山西',
+    '河南',
+    '江苏',
+    '安徽',
+    '浙江',
+    '江西',
+    '湖北',
+    '湖南',
+    '福建',
+    '广东',
+    '广西',
+    '海南',
+    '贵州',
+    '云南',
+    '四川',
+    '陕西',
+    '甘肃',
+    '宁夏',
+    '青海',
+    '新疆',
+    '西藏',
+    '香港',
+    '澳门',
+    '台湾',
+    '其它地区',
   );
 
   return $terms;
