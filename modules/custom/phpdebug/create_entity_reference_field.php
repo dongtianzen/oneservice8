cac63b7bd8dfe5ae2b5c5a888c3df6913713df3b
Dong Tian <dongtianzen@gmail.com>
2017-01-07 12:16:23 -0500

    dev _entity_create_fields

--- modules/custom/phpdebug/create_entity_reference_field.php
+++ modules/custom/phpdebug/create_entity_reference_field.php
@@ -8,20 +8,59 @@
  */
 
 
-function _entity_create_fields() {
-  entity_create('field_storage_config', array(
+function _run_batch_entity_create_fields() {
+  $entity_info = array(
+    'entity_type' => 'node',  // 'taxonomy_term', 'user'
+    'bundle' => 'page',
+  );
+
+  $fields = _entity_fields_info();
+  foreach ($fields as $field) {
+    _entity_create_fields($entity_info, $field);
+  }
+
+}
+
+function _entity_fields_info() {
+  $fields[] = array(
     'field_name' => 'field_page_large_text',
-    'entity_type' => 'node',
-    'type' => 'text_with_summary',    // 'entity_reference', 'image'
+  );
+
+  return $fields;
+}
+
+function _entity_create_fields($entity_info, $field) {
+  entity_create('field_storage_config', array(
+    'field_name'  => $field['field_name'],
+    'entity_type' => $entity_info['entity_type'],
+    'type'        => $field['type'],
   ))->save();
 
   entity_create('field_config', array(
-    'field_name' => 'field_page_large_text',
-    'label' => 'Large Text',
-    'entity_type' => 'node',   // 'taxonomy_term'
-    'bundle' => 'page',
+    'field_name'  => $field['field_name'],
+    'label'       => 'Large Text',
+    'entity_type' => $entity_info['entity_type'],
+    'bundle'      => $entity_info['bundle'],
     // optional
-    'required' => FALSE,
+    'required'    => FALSE,
+    'description' => t('your description'),
+  ))->save();
+}
+
+function _entity_create_field_template() {
+  entity_create('field_storage_config', array(
+    'field_name'  => 'field_page_large_text',
+    'entity_type' => 'node',          // 'taxonomy_term'
+    'type'        => 'text_with_summary',    // 'entity_reference', 'image'
+  ))->save();
+
+  entity_create('field_config', array(
+    'field_name'  => 'field_page_large_text',
+    'label'       => 'Large Text',
+    'entity_type' => 'node',          // 'taxonomy_term'
+    'bundle'      => 'page',
+    // optional
+    'required'    => FALSE,
     'description' => t('your description'),
   ))->save();
 }
