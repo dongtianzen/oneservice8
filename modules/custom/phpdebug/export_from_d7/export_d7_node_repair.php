41a73f1391cefc5ff3fc83fbf16e05a6ba259542
Dong Tian <dongtianzen@gmail.com>
2017-01-15 11:52:45 -0500

    debug _export_d7_node_repair

--- modules/custom/phpdebug/export_from_d7/export_d7_node_repair.php
+++ modules/custom/phpdebug/export_from_d7/export_d7_node_repair.php
@@ -21,20 +21,26 @@ function _export_d7_node_repair() {
         $row = array();
         foreach ($node_method_collections as $field_name => $value) {
 
-        //   $field_value = "NULL";
+          $field_value = "NULL";
+          if (isset($node->{$field_name}['und'][0][$value])) {
+            $field_value = $node->{$field_name}['und'][0][$value];
 
-        //   if (isset($node->$field_name['und'][0][$value]) {
-        //     $field_value = $node->{$field_name}['und'][0][$value];
+            if ($value == "tid" || $value == "target_id") {
+              $term = taxonomy_term_load($node->{$field_name}['und'][0][$value]);
+              if (isset($term->name)) {
+                $field_value = $term->name;
+              }
 
-        //     if ($value == "tid" || $value == "target_id")) {
-        //       $term = taxonomy_term_load($node->{$field_name}['und'][0][$value]);
-        //       if (isset($term->name)) {
-        //         $field_value = $term->name;
-        //       }
-        //     }
-        //   }
-        //   $row[] = $field_name . ' - ' . $field_value;
-        //   dpm($field_name . ' - ' . $field_value);
+              if ($field_name == "field_repair_check_staff") {
+                $user = user_load($node->{$field_name}['und'][0][$value]);
+                if (isset($user->name)) {
+                  $field_value = $user->name;
+                }
+              }
+            }
+          }
+          $row[] = $field_name . ' - ' . $field_value;
+          dpm($field_name . ' - ' . $field_value);
         }
 
         dpm('array(' . implode(',', $row) . '),');
