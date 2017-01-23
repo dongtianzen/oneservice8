6231b42b2737e1236d7de46ff185cf6edab432ff
Dong Tian <dongtianzen@gmail.com>
2017-01-23 10:12:19 -0500

    dev angularSnapshot

--- modules/custom/dashpage/src/Controller/DashpageController.php
+++ modules/custom/dashpage/src/Controller/DashpageController.php
@@ -19,6 +19,50 @@ class DashpageController extends ControllerBase {
   /**
    *
    */
+  public function adminTag() {
+    $admin_tags = Xss::getAdminTagList();
+    $admin_tags_plus = [
+      'canvas', 'form', 'input', 'label', 'md-button', 'md-content',
+      'md-input-container', 'md-menu', 'md-menu-content', 'md-option',
+      'md-select', 'md-tab', 'md-tabs', 'md-tooltip',
+    ];
+    $admin_tags = array_merge($admin_tags, $admin_tags_plus);
+
+    return $admin_tags;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function angularSnapshot() {
+    $DashpageContentGenerator = new DashpageContentGenerator();
+    $output = $DashpageContentGenerator->angularSnapshot();
+
+    $admin_tags = Xss::getAdminTagList();
+    $admin_tags_plus = [
+      'canvas', 'form', 'input', 'label', 'md-button', 'md-content',
+      'md-input-container', 'md-menu', 'md-menu-content', 'md-option',
+      'md-select', 'md-tab', 'md-tabs', 'md-tooltip',
+    ];
+    $admin_tags = array_merge($admin_tags, $admin_tags_plus);
+
+    $build = array(
+      '#type' => 'markup',
+      '#header' => 'header',
+      '#markup' => $output,
+      '#allowed_tags' => $this->adminTag(),
+      '#attached' => array(
+        'library' => array(
+          'dashpage/angular_snapshot',
+        ),
+      ),
+    );
+    return $build;
+  }
+
+  /**
+   *
+   */
   public function ContentGeneratorTemplate($method = 'template') {
     $DashpageContentGenerator = new DashpageContentGenerator();
     $output = $DashpageContentGenerator->{$method}();
