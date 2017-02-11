ca90a16a11dd95ae15d0a78f7008a5ae4897874c
Dong Tian <dongtianzen@gmail.com>
2017-02-11 18:17:13 -0500

    debug export_d7_term_from.php

--- modules/custom/phpdebug/export_from_d7/export_d7_term_from.php
+++ modules/custom/phpdebug/export_from_d7/export_d7_term_from.php
@@ -1,49 +1,39 @@
-
 <?php
 
 /**
  *
-  require_once(DRUPAL_ROOT . '/modules/custom/phpdebug/export_from_d7/export_d7_term_from.php');
-  _state_value(3);
+  from mildder8 run on lillyglobal
+  require_once('/Applications/AMPPS/www/mildder8/modules/custom/phpdebug/export_from_d7/export_d7_term.php');
+  _taxonomyGetTreeTidNames();
  */
 
-function _state_value($key = NULL) {
-}
+  /**
+   * NotificationPrimary
+   */
+  function _term_method_collections_7() {
+    $output['vid'] = 7;
+    $output['field_name'] = array(
+      array(
+        'd7_field_name' => 'field_notify_mail_to',
+        'd8_field_name' => 'field_notify_mailto',
+      ),
+      array(
+        'd7_field_name' => 'field_notify_mail_title',
+        'd8_field_name' => 'field_notify_mailtitle',
+      ),
+      array(
+        'd7_field_name' => 'field_notify_mail_content',
+        'd8_field_name' => 'field_notify_mailcontent',
+      ),
+      array(
+        'd7_field_name' => 'field_notify_ahead_time',
+        'd8_field_name' => 'field_notify_aheadtime',
+      ),
+      array(
+        'd7_field_name' => 'field_notify_enable_status',
+        'd8_field_name' => 'field_notify_enablestatus',
+      ),
+    );
 
-function taxonomyGetTreeTidNames($vid = NULL) {
-  $output = array();
-
-
-  $client_method_collections = _client_method_collections();
-  if ($vid) {
-    $terms = taxonomy_get_tree($vid);
-
-    if (is_array($terms)) {
-      foreach ($terms as $term) {
-        $output[$term->tid] = $term->name;
-        $term = taxonomy_term_load($term->tid);
-        dpm($term);
-
-        $row = array();
-        foreach ($client_method_collections as $key => $value) {
-          $row[] = $value;
-        }
-      }
-    }
+    return $output;
   }
-
-  return $output;
-}
-
-function _client_method_collections() {
-  $vid = 2;
-  $methods = array(
-    'field_client_address' => "field_client_contactaddress['und'][0]['safe_value']",
-    'field_client_clienttype' => "field_client_clienttype['und'][0]['target_id']",
-    'field_client_contactname' => "field_client_contactname['und'][0]['safe_value']",
-    'field_client_email' => "field_client_contactemail['und'][0]['safe_value']",
-    'field_client_phone' => "field_client_contactphone['und'][0]['safe_value']",
-    'field_client_province' => "field_client_province['und'][0]['tid']",
-    'field_client_salesperson' => "field_client_bundlesales['und'][0]['target_id']",
-  );
-}
