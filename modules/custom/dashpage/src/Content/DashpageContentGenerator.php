cfbaa28f18f16532620e829f8dfa4663c52f072b
Dong Tian <dongtianzen@gmail.com>
2017-02-20 11:02:53 -0500

    clean css

--- modules/custom/dashpage/src/Content/DashpageContentGenerator.php
+++ modules/custom/dashpage/src/Content/DashpageContentGenerator.php
@@ -56,10 +56,10 @@ class DashpageContentGenerator extends ControllerBase {
   public function angularSnapshot() {
     $output = '';
     $output .= '<div id="pageInfoBase" data-ng-app="pageInfoBase" class="custom-pageinfo pageinfo-subpage-common">';
-      $output .= '<div data-ng-controller="PageInfoBaseController" class="row margin-top-16">';
+      $output .= '<div data-ng-controller="PageInfoBaseController" class="row margin-0">';
 
-        $output .= '<div class="block-one bg-ffffff padding-bottom-20">';
-          $output .= '<div class="row">';
+        $output .= '<div class="block-one bg-ffffff margin-top-16">';
+          $output .= '<div class="">';
 
             $output .= '<div class="">';
               $output .= '<div class="float-left">';
@@ -68,7 +68,7 @@ class DashpageContentGenerator extends ControllerBase {
             $output .= '</div>';
 
             $output .= '<div class="margin-top-12">';
-              $output .= '<table class="table table-hover">';
+              $output .= '<table class="table table-hover table-responsive">';
                 $output .= '<thead>';
                   $output .= '<tr>';
                     $output .= '<th data-ng-repeat="(tableHeadKey, tableHeadCell) in pageData[0]">';
@@ -80,7 +80,7 @@ class DashpageContentGenerator extends ControllerBase {
                 $output .= '<tbody data-ng-repeat="tableRow in pageData | filter:inputFilter" class="">';
                   $output .= '<tr>';
                     $output .= '<td data-ng-repeat="tableRowCell in tableRow">';
-                      $output .= "{{ tableRowCell }}";
+                      $output .= '<span data-ng-bind-html="$sce.trustAsHtml(tableRowCell)">{{ tableRowCell }}</span>';
                     $output .= '</td>';
                   $output .= '</tr>';
 
