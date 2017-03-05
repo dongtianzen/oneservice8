bbefb9f72e8ee9c26e2c98974d6e449721410fd3
Dong Tian <dongtianzen@gmail.com>
2017-03-05 14:28:51 -0500

    dev landingPage

--- modules/custom/dashpage/src/Content/DashpageContentGenerator.php
+++ modules/custom/dashpage/src/Content/DashpageContentGenerator.php
@@ -122,77 +122,70 @@ class DashpageContentGenerator extends ControllerBase {
    * render views output
    */
   public function landingPage($nid = NULL) {
-    $FieldService = $this->flexinfoEntityService->getEntity('field');
-
     $output = '';
-
     $output .= '<div class="dashpage-wrapper">';
-      $output .= '<div class="">';
-        $output .= '产品保修期外维修单';
-      $output .= '</div>';
-      $output .= '<div class="">';
-        $output .= '贵公司送修的设备型号:';
-      $output .= '</div>';
-      $output .= '<div class="">';
-        $output .= '';
-      $output .= '</div>';
-      $output .= '<div class="">';
-        $output .= '已经超过保修期, 需要更换配件, 维修后仅对该设备配件将提供为期90天的保修服务.';
-      $output .= '</div>';
-      $output .= '<div class="">';
-        $output .= '维修情况如下:';
-      $output .= '</div>';
+      $output .= '<div class="row">';
 
-      $node  = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
-      if ($node) {
-        $output .= '<table class="table table-hover table-responsive color-449bb5 font-bold">';
-          $output .= '<thead>';
-            $output .= '<tr>';
-              $output .= '<td>#</td>';
-              $output .= '<td>型号</td>';
-              $output .= '<td>序列号</td>';
-              $output .= '<td>价格</td>';
-            $output .= '</tr>';
-          $output .= '</thead>';
-          $output .= '<tbody>';
-            $output .= '<tr>';
-              $output .= '<td>客户名称</td>';
-              $output .= '<td colspan="3">' . $FieldService->getFieldSingleValue('node', $node, 'field_quote_clientname') . '</td>';
-            $output .= '</tr>';
-          $output .= '</tbody>';
-        $output .= '</table>';
+        $output .= '<div class="col-xs-12 col-sm-6 col-md-4 min-height-216">';
+          $output .= '<div class="thumbnail">';
+            // $output .= '<img src="..." alt="...">';
+            $output .= '<i class="fa fa-calendar" aria-hidden="true"></i>';
+            $output .= '<div class="caption">';
+              $output .= '<h3>用户下载“万博维修申请表”</h3>';
+              $output .= '<p>用户下载“万博维修申请表”</p>';
+            $output .= '</div>';
+          $output .= '</div>';
+        $output .= '</div>';
 
-      }
-      else {
-        $output .= $this->t('没有发现维修信息');
-      }
+        $output .= '<div class="col-xs-12 col-sm-6 col-md-4 min-height-216">';
+          $output .= '<div class="thumbnail">';
+            // $output .= '<img src="..." alt="...">';
+            $output .= '<i class="fa fa-calendar" aria-hidden="true"></i>';
+            $output .= '<div class="caption">';
+              $output .= '<h3>填写申请表中的详细信息</h3>';
+              $output .= '<p>用户需要填写申请表中的详细信息，我司在维修受理后出具的发票及返修设备的返回地址均以此为依据（除特殊注明）； 用户需标注设备的故障现象，以方便我们进行初检；</p>';
+            $output .= '</div>';
+          $output .= '</div>';
+        $output .= '</div>';
 
-      $output .= '<div class="">';
-        $output .= '因已经超过保修期，需要支付维修费.费用为￥7000.00(人民币柒仟元整元整).维修后该设备相同部件将提供为期90天的保修服务。';
-      $output .= '</div>';
-      $output .= '<div class="">';
-        $output .= '以下是我司有关汇款信息:';
-      $output .= '</div>';
-      $output .= '<div class="margin-top-12">';
-        $output .= '名称: 北京万博信普通讯技术有限公司';
-      $output .= '</div>';
-      $output .= '<div class="">';
-        $output .= '开户行: 工商银行北京八里庄支行';
-      $output .= '</div>';
-      $output .= '<div class="">';
-      $output .= '帐号: 0200003819020117350';
-      $output .= '</div>';
-      $output .= '<div class="margin-top-12">';
-        $output .= '收到维修费后，北京万博信普通讯技术有限公司负责开具发票，并将设备快递回贵公司。';
-      $output .= '</div>';
-      $output .= '<div class="">';
-        $output .= '李靖';
-      $output .= '</div>';
-      $output .= '<div class="">';
-        $output .= $FieldService->getFieldSingleValue('node', $node, 'field_quote_date');
-      $output .= '</div>';
-      $output .= '<div class="btn btn-success quote-node-print-button hidden-print margin-top-48 clear-both" type="button">';
-        $output .= 'Print';
+        $output .= '<div class="col-xs-12 col-sm-6 col-md-4 min-height-216">';
+          $output .= '<div class="thumbnail">';
+            // $output .= '<img src="..." alt="...">';
+            $output .= '<i class="fa fa-calendar" aria-hidden="true"></i>';
+            $output .= '<div class="caption">';
+              $output .= '<h3>申请表确认</h3>';
+              $output .= '<p>用户以传真、邮件等形式将维修申请表发给我司进行确认（加盖公章或签名）；</p>';
+            $output .= '</div>';
+          $output .= '</div>';
+        $output .= '</div>';
+
+        $output .= '<div class="col-xs-12 col-sm-6 col-md-4 min-height-216">';
+          $output .= '<div class="thumbnail">';
+            // $output .= '<img src="..." alt="...">';
+            $output .= '<i class="fa fa-calendar" aria-hidden="true"></i>';
+            $output .= '<div class="caption">';
+              $output .= '<h3>快递单号填写</h3>';
+              $output .= '<p>用户将发至我司的快递单号填写在维修申请表的“物流编码”中，以方便我司查询</p>';
+            $output .= '</div>';
+          $output .= '</div>';
+        $output .= '</div>';
+
+        $output .= '<div class="col-xs-12 col-sm-6 col-md-4 min-height-216">';
+          $output .= '<div class="thumbnail">';
+            // $output .= '<img src="..." alt="...">';
+            $output .= '<i class="fa fa-calendar" aria-hidden="true"></i>';
+            $output .= '<div class="caption">';
+              $output .= '<h3>维修周期为15工作日</h3>';
+              $output .= '<p> 我司的处理流程为 接收、初验、维修和返回；通常故障维修周期为15工作日。        A 接收，我司接收到待修设备后，用户即可在我网站根据序列号查询到该设备的状态；
+         B 初验、维修等流程情况与用户填写不符时，我司会按照申请单中的信息与用户联系；
+         C 我司会按申请单中的信息发送维修报价表，如无特殊情况，我们的报价仅包含单次国内的运输费用；
+         D 用户付款后，我司开始实施维修；
+         E 维修结束后，出具设备的维修报告，对设备的延保日期进行贴示；
+         F 我司会按照申请单中的地址给用户返还设备；</p>';
+            $output .= '</div>';
+          $output .= '</div>';
+        $output .= '</div>';
+
       $output .= '</div>';
     $output .= '</div>';
 
