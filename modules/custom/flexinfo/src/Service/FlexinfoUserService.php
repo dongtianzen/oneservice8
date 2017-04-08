d027db369365e68709ab513202a0fafa64ba2222
Dong Tian <dongtianzen@gmail.com>
2017-04-08 10:42:59 -0400

    dev FlexinfoUserService

--- modules/custom/flexinfo/src/Service/FlexinfoUserService.php
+++ modules/custom/flexinfo/src/Service/FlexinfoUserService.php
@@ -12,13 +12,68 @@ namespace Drupal\flexinfo\Service;
  */
 class FlexinfoUserService {
 
+  /**
+   *
+   */
+  public function checkUserMailExist($user_mail = NULL) {
+    $output = FALSE;
+
+    if ($user_mail) {
+      $user_uid = $this->getUidByMail($user_mail);
+      if ($user_uid) {
+        $output = TRUE;
+      }
+    }
+
+    return $output;
+  }
+
+  /**
+   *
+   */
+  public function checkUserNameExist($user_name = NULL) {
+    $output = FALSE;
+
+    if ($user_name) {
+      $user_uid = $this->getUidByUserName($user_name);
+      if ($user_uid) {
+        $output = TRUE;
+      }
+    }
+
+    return $output;
+  }
+
+  public function getUidByMail($mail = NULL) {
+    $output = NULL;
+
+    if ($mail) {
+      $user = user_load_by_mail($mail);
+      if ($user) {
+        $output = $user->id();
+      }
+      else {
+        if (\Drupal::currentUser()->id() == 1) {
+          dpm('not found this user email  - ' . $mail . ' - getUidByMail()');
+        }
+      }
+    }
+
+    return $output;
+  }
+
   public function getUidByUserName($user_name = NULL) {
     $output = NULL;
 
     if ($user_name) {
       $user = user_load_by_name($user_name);
-      if (count($user) > 0) {
-        $output = $user->get('uid')->value;
+      if ($user) {
+        $output = $user->id();
+      }
+      else {
+        if (\Drupal::currentUser()->id() == 1) {
+          dpm('not found this user name - ' . $user_name . ' - getUidByUserName()');
+        }
       }
     }
 
@@ -31,13 +86,15 @@ class FlexinfoUserService {
   public function getUserNameByUid($uid = NULL) {
     $output = NULL;
 
-    $account = \Drupal\user\Entity\User::load($uid);
-    if ($account) {
-      $output = $account->getUsername();
-      $output = $account->get('name')->value;
+    if ($uid) {
+      $user = \Drupal::entityTypeManager()->getStorage('user')->load($uid);
+      if ($user) {
+        $output = $user->getUsername();
+      }
     }
 
     return $output;
   }
 
+  /** - - - - - - Field - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 }
