a7780bc7ec86793de577683c71e954e72ec54216
Dong Tian <dongtianzen@gmail.com>
2017-06-26 22:37:52 -0400

    cc

--- modules/custom/superinfo/src/Controller/SuperinfoController.php
+++ modules/custom/superinfo/src/Controller/SuperinfoController.php
@@ -52,7 +52,6 @@ class SuperinfoController extends ControllerBase {
       ->setEntity($entity);
     $form = \Drupal::formBuilder()->getForm($entity_form);
 
-
     // default_value
     // $node  = \Drupal::entityTypeManager()->getStorage('node')->load(468);
     // $form['field_repair_requestnode']['widget'][0]['target_id']['#default_value'] = $node;
